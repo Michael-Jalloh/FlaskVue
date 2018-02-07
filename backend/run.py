0a25d26f00426fa1b3709284bfe81f2108e05bd3
Michael Jalloh <michaeljalloh19@gmail.com>
2018-02-07 14:51:41 +0000

    a

---
+++ backend/run.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__,
+            static_folder = "../dist/static",
+            template_folder = "../dist")
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/yo')
+def yo():
+    return "Hello world"
+
+@app.route('/', defaults={'path':""})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template('index.html')
---
+++ frontend/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-aside width="200px" style="background:red;">Aside</el-aside>
+      <el-container>
+        <el-header style="background:green">Header</el-header>
+        <el-main>
+          <router-view/>
+        </el-main>
+        <el-footer style="background:blue;">Footer</el-footer>
+      </el-container>
+
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+p{
+  margin: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  //margin-top: 60px;
+  //background-color: black;
+  height: 100%;
+  width: 100%;
+}
+</style>
